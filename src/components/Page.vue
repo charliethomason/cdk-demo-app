befccb5256cec0c1f6abb975531014d503fa0796
charliethomason <thomason.charlie@gmail.com>
2019-11-06 13:51:19 -0600

    added cdkGlobalHeader and cdkSIdeNav

--- src/components/Page.vue
+++ src/components/Page.vue
@@ -1,14 +1,51 @@
 <template>
-    <h1>{{ message }}</h1>
+    <main>
+        <cdk-global-header v-bind="headerProps" />
+        <div class="demo-wrapper">
+            <cdk-side-nav v-bind="sideNavProps" />
+        </div>
+    </main>
 </template>
 
 <script>
+import cdkGlobalHeader from '@uptake/cdk-vue/cdkGlobalHeader';
+import cdkSideNav from '@uptake/cdk-vue/cdkSideNav';
+
+import { header, sideNav } from '../data/content.json';
+
 export default {
     name: 'Page',
+    components: {
+        cdkGlobalHeader,
+        cdkSideNav
+    },
     data() {
         return {
-            message: 'CDK Demo App'
+            header,
+            sideNav
         };
+    },
+    computed: {
+        headerProps() {
+            const {
+                productName,
+                appSwitcherHeader,
+                appSwitcherItems
+            } = this.header;
+            return {
+                productName,
+                appSwitcherHeader,
+                appSwitcherItems
+            };
+        },
+        sideNavProps() {
+            const {
+                initialItems
+            } = this.sideNav;
+            return {
+                initialItems
+            };
+        }
     },
     methods: {
         getMovies() {
